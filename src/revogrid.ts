a5879743c5a2dd574d46e91a95e3fdc78f6838b8
revolist <maksim.k@revolist.eu>
2020-09-08 15:13:13 +0300

    edit updates

--- src/revogrid.ts
+++ src/revogrid.ts
@@ -13,6 +13,5 @@ export const RevogrEdit = /*@__PURE__*/createReactComponent<JSX.RevogrEdit, HTML
 export const RevogrHeader = /*@__PURE__*/createReactComponent<JSX.RevogrHeader, HTMLRevogrHeaderElement>('revogr-header');
 export const RevogrOverlaySelection = /*@__PURE__*/createReactComponent<JSX.RevogrOverlaySelection, HTMLRevogrOverlaySelectionElement>('revogr-overlay-selection');
 export const RevogrScrollVirtual = /*@__PURE__*/createReactComponent<JSX.RevogrScrollVirtual, HTMLRevogrScrollVirtualElement>('revogr-scroll-virtual');
-export const RevogrTextEditor = /*@__PURE__*/createReactComponent<JSX.RevogrTextEditor, HTMLRevogrTextEditorElement>('revogr-text-editor');
 export const RevogrViewport = /*@__PURE__*/createReactComponent<JSX.RevogrViewport, HTMLRevogrViewportElement>('revogr-viewport');
 export const RevogrViewportScroll = /*@__PURE__*/createReactComponent<JSX.RevogrViewportScroll, HTMLRevogrViewportScrollElement>('revogr-viewport-scroll');
