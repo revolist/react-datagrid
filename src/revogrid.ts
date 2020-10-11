f1dd0105801482d44af7e344c25f64092ffc5c9e
revolist <maksim.k@revolist.eu>
2020-10-11 15:43:33 +0300

    updates

--- src/revogrid.ts
+++ src/revogrid.ts
@@ -8,11 +8,14 @@ import type { JSX } from '@revolist/revogrid';
 
 
 export const RevoGrid = /*@__PURE__*/createReactComponent<JSX.RevoGrid, HTMLRevoGridElement>('revo-grid');
+export const RevogrClipboard = /*@__PURE__*/createReactComponent<JSX.RevogrClipboard, HTMLRevogrClipboardElement>('revogr-clipboard');
 export const RevogrData = /*@__PURE__*/createReactComponent<JSX.RevogrData, HTMLRevogrDataElement>('revogr-data');
 export const RevogrEdit = /*@__PURE__*/createReactComponent<JSX.RevogrEdit, HTMLRevogrEditElement>('revogr-edit');
+export const RevogrFocus = /*@__PURE__*/createReactComponent<JSX.RevogrFocus, HTMLRevogrFocusElement>('revogr-focus');
 export const RevogrHeader = /*@__PURE__*/createReactComponent<JSX.RevogrHeader, HTMLRevogrHeaderElement>('revogr-header');
 export const RevogrOrderEditor = /*@__PURE__*/createReactComponent<JSX.RevogrOrderEditor, HTMLRevogrOrderEditorElement>('revogr-order-editor');
 export const RevogrOverlaySelection = /*@__PURE__*/createReactComponent<JSX.RevogrOverlaySelection, HTMLRevogrOverlaySelectionElement>('revogr-overlay-selection');
 export const RevogrScrollVirtual = /*@__PURE__*/createReactComponent<JSX.RevogrScrollVirtual, HTMLRevogrScrollVirtualElement>('revogr-scroll-virtual');
+export const RevogrTempRange = /*@__PURE__*/createReactComponent<JSX.RevogrTempRange, HTMLRevogrTempRangeElement>('revogr-temp-range');
 export const RevogrViewport = /*@__PURE__*/createReactComponent<JSX.RevogrViewport, HTMLRevogrViewportElement>('revogr-viewport');
 export const RevogrViewportScroll = /*@__PURE__*/createReactComponent<JSX.RevogrViewportScroll, HTMLRevogrViewportScrollElement>('revogr-viewport-scroll');
