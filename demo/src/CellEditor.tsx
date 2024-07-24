5792f4aacc0f03310e23a2fd4124b0a1186e0a0f
maks <mojpo4tovik@gmail.com>
2024-07-24 19:47:48 +0100

    docs: advance mounted demo

--- demo/src/CellEditor.tsx
+++ demo/src/CellEditor.tsx
@@ -1,8 +1,14 @@
+import { forwardRef, useEffect } from 'react';
 import type { EditorType } from '@revolist/react-datagrid';
 
 /**
  * Custom editor component
  */
-export const ButtonEditor = ({ close, column }: EditorType) => {
+export const ButtonEditor = forwardRef(({ close, column }: EditorType, ref) => {
+  useEffect(() => {
+    console.log('Editor did mount');
+  });
+
   return <button onClick={() => close()}>Close {column.value}</button>;
-};
+});
+
